889b8e2a6a58ac35da2c91ff370c94d0ce6c657f
rzhang57 <48890532+rzhang57@users.noreply.github.com>
2025-12-02 20:42:49 -0800

    groq wrapper

---
+++ src/test/java/com/groq/wrapper/WrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groq.wrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

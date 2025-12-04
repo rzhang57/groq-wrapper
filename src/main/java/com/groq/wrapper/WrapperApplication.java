d1639915b50d033afa32f789417964039ae95145
rzhang57 <48890532+rzhang57@users.noreply.github.com>
2025-12-03 21:35:20 -0800

    add env variables and app is in running state

--- src/main/java/com/groq/wrapper/WrapperApplication.java
+++ src/main/java/com/groq/wrapper/WrapperApplication.java
@@ -2,12 +2,12 @@ package com.groq.wrapper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class WrapperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WrapperApplication.class, args);
 	}
-
 }
